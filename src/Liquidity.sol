9d692c83ea47c0029874c32c101112f2b330460c
Komron Khisomov <normok_9595@bk.ru>
2024-08-22 14:32:14 +0500

    INonfungiblePositionManager

--- src/Liquidity.sol
+++ src/Liquidity.sol
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.20;
 
+import {INonfungiblePositionManager} from "./interfaces/INonfungiblePositionManager.sol";
+import {IERC20} from "../lib/openzeppelin-contracts/contracts/interfaces/IERC20.sol";
+
 contract Liquidity {
     
 }
