564ff58027a5db3611e8087f16cb1aa0a56826ef
Komron Khisomov <normok_9595@bk.ru>
2024-08-22 14:39:48 +0500

    beginning Liquidity

--- src/Liquidity.sol
+++ src/Liquidity.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.20;
 
 import {INonfungiblePositionManager} from "./interfaces/INonfungiblePositionManager.sol";
+import {IWETH} from "./interfaces/IWETH.sol";
 import {IERC20} from "../lib/openzeppelin-contracts/contracts/interfaces/IERC20.sol";
 import {IERC721Receiver} from "../lib/openzeppelin-contracts/contracts/interfaces/IERC721Receiver.sol";
 
@@ -11,4 +12,8 @@ address constant DAI = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
 contract Liquidity is IERC721Receiver{
     IERC20 private constant dai = IERC20(DAI);
     IWETH private constant weth = IWETH(WETH);
+
+    int24 private constant MIN_TICK = -887272;
+    int24 private constant MAX_TICK = -MIN_TICK;
+    int24 private constant TICK_SPACING = 60;
 }
