1f4ae2b8d4e37810a580a2a0d84ecc922d0394a9
Komron Khisomov <normok_9595@bk.ru>
2024-08-23 18:48:24 +0500

    flash

--- src/FlashLoan.sol
+++ src/FlashLoan.sol
@@ -1,6 +1,35 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.20;
 
+import {IUniswapV3Pool} from "../lib/v3-core/contracts/interfaces/IUniswapV3Pool.sol";
+import {IERC20} from "../lib/openzeppelin-contracts/contracts/interfaces/IERC20.sol";
+
 contract FlashLoan {
-    
+    struct FlashCallbackData {
+        uint256 amount0;
+        uint256 amount1;
+        address caller;
+    }
+
+    IUniswapV3Pool private immutable pool;
+    IERC20 private immutable token0;
+    IERC20 private immutable token1;
+
+    constructor(address _pool) {
+        pool = IUniswapV3Pool(_pool);
+        token0 = IERC20(pool.token0);
+        token1 = IERC20(pool.token1);
+    }
+
+    function flash(uint256 amount0, uint256 amount1) external {
+        bytes memory data = abi.encode(
+            FlashCallbackData({
+                amount0: amount0,
+                amount1: amount1,
+                caller: msg.sender
+            })
+        );
+
+        IUniswapV3Pool(pool).flash(address(this), amount0, amount1, data);
+    }
 }
